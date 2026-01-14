6d5ee8e7c5a51897dcbecf75240f8eb67466f342
Karthik-bn151193 <karthik.bn15111993@gmail.com>
2026-01-14 06:32:14 +0000

    first commit

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
 
+resource "aws_s3_bucket" "example" {
+  bucket = "terraformtestbucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
